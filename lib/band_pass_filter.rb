2d2896ead0d1b9acf5bf4c81fc130561615e1732
dbacall <dbacall@hotmail.co.uk>
2020-01-31 09:23:13 +0000

    feature: lower limit filter can change 2 sounds

--- lib/band_pass_filter.rb
+++ lib/band_pass_filter.rb
@@ -1,4 +1,4 @@
 
 def lower_limit_filter(limit, sounds)
-  [80]
+  sounds.map { |sound| sound = limit if sound < limit }
 end
--- spec/band_pass_filter_spec.rb
+++ spec/band_pass_filter_spec.rb
@@ -4,4 +4,9 @@ describe '#lower_limit_filter' do
   it 'changes the sound frequency to the lower limit for one sound wave' do
     expect(lower_limit_filter(80, [70])).to eq [80]
   end
+
+  it 'changes the sound frequency to teh lower limit for two sound waves' do
+    expect(lower_limit_filter(80, [70, 60])).to eq [80, 80]
+
+  end
 end
